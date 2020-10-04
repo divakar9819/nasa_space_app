918a890866eda51cfed2800617abee841a2376c2
divakar9819 <53227365+divakar9819@users.noreply.github.com>
2020-10-04 22:35:45 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+<title>About</title>
+</head>
+<body bgcolor="yellow">
+<h1>What Is NASA?<h1>
+<h3>NASA stands for National Aeronautics and Space Administration. NASA is a U.S. government agency that is responsible for science and technology related to air and space. The Space Age started in 1957 with the launch of the Soviet satellite Sputnik.
+NASA opened for business on Oct. 1, 1958. The agency was created to oversee U.S. space exploration and aeronautics research.
+The administrator is in charge of NASA. The NASA administrator is nominated by the president and confirmed by a vote in the Senate.
+</h3>
+
+<h1>NASA Leadership</h1>
+<h3>Administrator Jim Bridenstine
+Portrait of Jim Bridenstine
+NASA Administrator Jim Bridenstine, Official Portrait, Wednesday, July 24, 2019, NASA Headquarters in Washington.
+Credits: NASA/Bill Ingalls
+James Frederick “Jim” Bridenstine was nominated by President Donald Trump, confirmed by the U.S. Senate, and sworn in as NASA’s 13th administrator on April 23, 2018. Bridenstine was elected in 2012 to represent Oklahoma’s First Congressional District. In Congress he served on the Armed Services Committee and the Science, Space and Technology Committee. Bridenstine’s career in federal service began in the U.S. Navy, flying the E-2C Hawkeye off the USS Abraham Lincoln aircraft carrier. He flew combat missions in Iraq and Afghanistan and accrued most of his 1,900 flight hours and 333 carrier-arrested landings.
+<h3>
+<h1>Deputy Administrator James W. Morhard</h1>
+<h3>James Morhard was nominated by President Trump and confirmed to be NASA’s 14th Deputy Administrator. He was sworn in on October 17th, 2018.
+
+Official Portrait of Jim Morhard
+Portrait, Jim Morhard, Deputy Administrator, National Aeronautics and Space Administration (NASA), Wednesday, Oct. 24, 2018 at NASA Headquarters in Washington.
+Credits: NASA/Bill Ingalls
+Jim helps provide overall leadership, planning, and policy direction. He performs duties and exercises powers delegated by the Administrator, assists him in making final agency decisions, and acts in his absence to govern NASA operations. Jim also is responsible for articulating and representing the agency's vision.
+</h3>
+<h1>NASA Centers and Facilities</h1>
+<h3>Ames Research Center
+View web site
+Visitors information
+Armstrong Flight Research Center
+View web site
+Visitors information
+Glenn Research Center
+View web site
+Visitors information
+Goddard Space Flight Center
+View web site
+Visitors information
+</h3>
+
+</body>
+</html>
---
+++ mission.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title>Mission</title>
+</head>
+<body >
+<h1><center>Important missions of NASA</center></h1>
+<h2>Pioneer</h2>
+<p>Pioneer 10 and Pioneer 11, launched in 1972 and 1973 respectively, 
+were the first spacecraft to visit the solar system's most photogenic gas giants,
+ Jupiter and Saturn. Pioneer 10 was the first probe to travel through the solar system's 
+ asteroid belt, a field of orbiting rocks between Mars and Jupiter. Then about a year-and-a-half 
+ after its launch, the spacecraft made the first flyby of the planet Jupiter.
+ It took stunning up-close photos of the Great Red Spot and the wide swaths of red that band the 
+ planet. About a year later, Pioneer 11 flew by Jupiter, and then moved on to Saturn, where 
+ it discovered a couple of previously unknown small moons around the planet, and a new ring. 
+ Both probes have stopped sending data, and are continuing out on their one-way voyages beyond the
+ solar system.</p>
+ <h2>Voyager</h2>
+ <p>Shortly after the Pioneers made their flybys, the Voyager1 and Voyager 2 probes followed. They made many important discoveries about Jupiter and Saturn, including rings around Jupiter and the presence of volcanism on Jupiter's moon, Io. Voyager went on to make the first flybys of Uranus, where it discovered 10 new moons, and Neptune, where it found that Neptune actually weighs less than astronomers thought. Both Voyager crafts have enough power to keep transmitting radio signals until at least 2025, and are now exploring the very edge of the solar system and beginning of interstellar space. Voyager 2 is currently the farthest man-made object from Earth, at more than a hundred times the distance from the Earth to the sun, and more than twice as far as Pluto.</p>
+
+<h2>WMAP</h2>
+<p>The Wilkinson Microwave Anisotropy Probe (WMAP), launched in 2001, may not be as well-known, but it measures with unprecedented accuracy the temperature of the radiation left over from the Big Bang. By mapping out the fluctuations in the so-called cosmic microwave background radiation, the spacecraft has heralded a leap forward in cosmological theories about the nature and origin of the universe. Among other revelations, the data from WMAP revealed a much more precise estimate for the age of the universe is 13.7 billion years and confirmed that about 95 percent of it is composed of poorly understood things called dark matter and dark energy.</p>
+<h2>Spirit & Opportunity</h2>
+<p>Intended for just a 90-day mission, these workhorse Mars rovers have far outdone themselves, and are still chugging away on the red planet more than five years after landing. Spirit and Opportunity, the twin Mars Exploration Rovers, landed on opposite sides of the planet in January2004. Since then, they have been traveling all over the surface, poking into craters and roving over unexplored hills. Among their major finds is evidence that the surface of Mars once had liquid water. (A tip of the hat to Sojourner rover, which brought full-colour close-ups of Mars in 1997, just as the Internet was becoming wildly popular, thereby earning a special place in the hearts of millions who enjoyed unprecedented access to NASA mission photos.)</p>
+
+</body>
+</html>
